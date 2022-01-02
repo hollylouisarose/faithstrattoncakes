87903096486a379ddd0c0aef7fe7a386eeab22e3
hollylouisarose <hlrstratton@gmail.com>
2022-01-02 16:09:10 +0000

    header styles added

--- header.php
+++ header.php
@@ -49,8 +49,7 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'faithstrattoncakes' ); ?></button>
+		<nav id="site-navigation" class="main-navigation">	
 			<?php
 			wp_nav_menu(
 				array(
