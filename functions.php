b1886fb0b5c288704ad791b7fb2de7538683d49f
hollylouisarose <hlrstratton@gmail.com>
2022-01-02 10:59:41 +0000

    linked up custom style sheet

--- functions.php
+++ functions.php
@@ -143,6 +143,8 @@ function faithstrattoncakes_scripts() {
 	wp_enqueue_style( 'faithstrattoncakes-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'faithstrattoncakes-style', 'rtl', 'replace' );
 
+  wp_enqueue_style('faithstrattoncakes-custom', get_template_directory_uri() . '/css/custom.css');
+
 	wp_enqueue_script( 'faithstrattoncakes-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
